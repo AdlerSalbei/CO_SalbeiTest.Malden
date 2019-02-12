1ab2f4dd7339e20c4ddb436f2c1d979fbf971feb
Salbei <salbei@gruppe-adler.de>
2019-02-13 00:04:52 +0100

    Add files via upload

---
+++ USER/missionInfo.hpp
@@ -0,0 +1,10 @@
+/*
+*   Legt allgemeine Information über die Mission fest.
+*/
+
+author = "Ainer & Salbei für Gruppe Adler";                                               // Missionsersteller
+onLoadName = "COOP TEMPLATE";                                                   // Name der Mission
+onLoadMission = "Sichern Sie einen Knotenpunkt.";                                                             // Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
+loadScreen = "data\loadpic.paa";                                                // Ladebild
+overviewPicture = "";                                                           // Bild, das in der Missionsauswahl angezeigt wird
+overviewText = "COOP TEST";                                                              // Text, der in der Missionsauswahl angezeigt wird
